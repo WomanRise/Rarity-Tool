7fdffb63f0155580d243c36cac8951dd9adf4331
Sodi <tassaduq009@gmail.com>
2025-01-31 01:31:51 +0500

    updates to serve images on localhost

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ if (app.get('env') === 'development') {
   }));
 }
 
-// view engine setup
+// View engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -26,8 +26,14 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
+// Serve static files from "public" and allow access via "/public/"
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
+// Debug log to confirm public folder is correctly set
+console.log("Serving static files from:", path.join(__dirname, 'public'));
+
+// Global variables for templates
 app.locals.app_name = config.app_name;
 app.locals.ga_code = config.ga;
 app.locals.collection_contract_address = config.collection_contract_address;
@@ -38,21 +44,21 @@ app.locals.content_image_frame = config.content_image_frame;
 app.locals.item_path_name = config.item_path_name;
 app.locals.use_wallet = config.use_wallet;
 
+// Routes
 app.use('/', indexRouter);
-app.use('/'+config.item_path_name, punksRouter);
+app.use('/' + config.item_path_name, punksRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
+// Catch 404 and forward to error handler
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+// Error handler
+app.use(function (err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
+  // Render the error page
   res.status(err.status || 500);
   res.render('error');
 });
